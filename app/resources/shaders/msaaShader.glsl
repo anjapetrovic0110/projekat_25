19cd36e614539aa06a0954946518a01f050453a0
petrovicanja0110 <petrovic.anja0110@gmail.com>
2026-05-18 14:19:22 +0200

    Add basic post-processing color grading

--- app/resources/shaders/msaaShader.glsl
+++ app/resources/shaders/msaaShader.glsl
@@ -20,5 +20,6 @@ in vec2 TexCoords;
 uniform sampler2D screenTexture;
 void main() {
     vec3 color = texture(screenTexture, TexCoords).rgb;
+    color = (color - 0.5) * 1.2 + 0.5;
     FragColor = vec4(color, 1.0);
 }
